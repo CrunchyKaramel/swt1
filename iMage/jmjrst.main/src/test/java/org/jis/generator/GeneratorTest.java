502ce55d79ae3e569aada634b441efbc63be78d6
Joshua Eilebrecht <joshua_eilebrecht@web.de>
2017-05-04 20:18:44 +0200

    Created Generator test.
    Created a generator test, with the before() and after() method implemented. It also requires a resource (picture.jpg), which is located in test/resource/org/jis/generator.

---
+++ iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
@@ -0,0 +1,59 @@
+package org.jis.generator;
+
+//todo reduce import 
+import static org.junit.Assert.fail;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReadParam;
+import javax.imageio.ImageReader;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.ImageInputStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class GeneratorTest {
+
+	@Before
+	public void setUp() throws Exception {
+		Generator testGenerator = new Generator(null, 0);
+		
+		//puts a picture in a BufferedImage object
+		URL url = getClass().getResource("picture.jpg");
+		File file = new File(url.getPath());
+		BufferedImage i = null;
+	    IIOMetadata imeta = null;
+
+	    try
+	    {
+	      ImageInputStream iis = ImageIO.createImageInputStream(file);
+	      ImageReader reader = ImageIO.getImageReadersByFormatName("jpg").next();
+	      reader.setInput(iis, true);
+	      ImageReadParam params = reader.getDefaultReadParam();
+	      i = reader.read(0, params);
+	      imeta = reader.getImageMetadata(0);
+	    }
+	    catch (IOException e)
+	    {
+	      System.err.println("Error while reading File: " + file.getAbsolutePath());
+	      e.printStackTrace();
+	      return;
+	    }
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
