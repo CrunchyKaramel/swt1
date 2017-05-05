f1ce935d423855b5672e167866cffdff9471ec26
Joshua Eilebrecht <joshua_eilebrecht@web.de>
2017-05-05 12:42:09 +0200

    Added Javadoc comment to setup(), formated GeneratorTest and Added save action.
    Added a Javadoc comment to clarify to why it exists, formated the class to get rid of any unwanted code style errors and also changed the setting so an edited class now gets automatically formated.
    This means that any formating will no longer be documented in any further commits.

--- iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
+++ iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
@@ -19,36 +19,39 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class GeneratorTest {
-	
-	//all necessary objects for the test
+
+	// all necessary objects for the test
 	Generator testGenerator;
 	BufferedImage i;
 
+	/**
+	 * Creates all necessary objects for the tests.
+	 * 
+	 * @throws Exception
+	 *             any type of exception that may occur.
+	 */
 	@Before
 	public void setUp() throws Exception {
 		testGenerator = new Generator(null, 0);
-		
-		//puts a picture in a BufferedImage object
+
+		// puts a picture in a BufferedImage object
 		URL url = getClass().getResource("picture.jpg");
 		File file = new File(url.getPath());
 		i = null;
-	    IIOMetadata imeta = null;
+		IIOMetadata imeta = null;
 
-	    try
-	    {
-	      ImageInputStream iis = ImageIO.createImageInputStream(file);
-	      ImageReader reader = ImageIO.getImageReadersByFormatName("jpg").next();
-	      reader.setInput(iis, true);
-	      ImageReadParam params = reader.getDefaultReadParam();
-	      i = reader.read(0, params);
-	      imeta = reader.getImageMetadata(0);
-	    }
-	    catch (IOException e)
-	    {
-	      System.err.println("Error while reading File: " + file.getAbsolutePath());
-	      e.printStackTrace();
-	      return;
-	    }
+		try {
+			ImageInputStream iis = ImageIO.createImageInputStream(file);
+			ImageReader reader = ImageIO.getImageReadersByFormatName("jpg").next();
+			reader.setInput(iis, true);
+			ImageReadParam params = reader.getDefaultReadParam();
+			i = reader.read(0, params);
+			imeta = reader.getImageMetadata(0);
+		} catch (IOException e) {
+			System.err.println("Error while reading File: " + file.getAbsolutePath());
+			e.printStackTrace();
+			return;
+		}
 	}
 
 	@After
