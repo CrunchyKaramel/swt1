7306e32db88dbdc7c307eb6ba8cf3a4ccf8d7d98
Joshua Eilebrecht <joshua_eilebrecht@web.de>
2017-05-05 17:12:01 +0200

    Changed @Before and @After and edited setup().
    Changed @Before to @BeforeClass as the loaded/created objects do not change between tests.
    Added code to load in the 90 degree rotated variant of picture.jpg and the 270 degree rotated one.

--- iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
+++ iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
@@ -13,15 +13,17 @@ import javax.imageio.ImageReader;
 import javax.imageio.metadata.IIOMetadata;
 import javax.imageio.stream.ImageInputStream;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class GeneratorTest {
 
 	// all necessary objects for the test
 	Generator testGenerator;
-	BufferedImage i;
+	BufferedImage picture;
+	BufferedImage picture90deg;
+	BufferedImage picture270deg;
 
 	/**
 	 * Creates all necessary objects for the tests.
@@ -29,14 +31,14 @@ public class GeneratorTest {
 	 * @throws Exception
 	 *             any type of exception that may occur.
 	 */
-	@Before
+	@BeforeClass
 	public void setUp() throws Exception {
 		testGenerator = new Generator(null, 0);
 
-		// puts a picture in a BufferedImage object
+		// reads in the original picture
 		URL url = getClass().getResource("picture.jpg");
 		File file = new File(url.getPath());
-		i = null;
+		picture = null;
 		IIOMetadata imeta = null;
 
 		try {
@@ -44,7 +46,44 @@ public class GeneratorTest {
 			ImageReader reader = ImageIO.getImageReadersByFormatName("jpg").next();
 			reader.setInput(iis, true);
 			ImageReadParam params = reader.getDefaultReadParam();
-			i = reader.read(0, params);
+			picture = reader.read(0, params);
+			imeta = reader.getImageMetadata(0);
+		} catch (IOException e) {
+			System.err.println("Error while reading File: " + file.getAbsolutePath());
+			e.printStackTrace();
+			return;
+		}
+		// reads in the 90 degree rotated picture
+		url = getClass().getResource("picture90deg.jpg");
+		file = new File(url.getPath());
+		picture90deg = null;
+		imeta = null;
+
+		try {
+			ImageInputStream iis = ImageIO.createImageInputStream(file);
+			ImageReader reader = ImageIO.getImageReadersByFormatName("jpg").next();
+			reader.setInput(iis, true);
+			ImageReadParam params = reader.getDefaultReadParam();
+			picture90deg = reader.read(0, params);
+			imeta = reader.getImageMetadata(0);
+		} catch (IOException e) {
+			System.err.println("Error while reading File: " + file.getAbsolutePath());
+			e.printStackTrace();
+			return;
+		}
+
+		// reads in the 270 degree rotated picture
+		url = getClass().getResource("picture270deg.jpg");
+		file = new File(url.getPath());
+		picture270deg = null;
+		imeta = null;
+
+		try {
+			ImageInputStream iis = ImageIO.createImageInputStream(file);
+			ImageReader reader = ImageIO.getImageReadersByFormatName("jpg").next();
+			reader.setInput(iis, true);
+			ImageReadParam params = reader.getDefaultReadParam();
+			picture270deg = reader.read(0, params);
 			imeta = reader.getImageMetadata(0);
 		} catch (IOException e) {
 			System.err.println("Error while reading File: " + file.getAbsolutePath());
@@ -53,7 +92,7 @@ public class GeneratorTest {
 		}
 	}
 
-	@After
+	@AfterClass
 	public void tearDown() throws Exception {
 	}
 
@@ -62,7 +101,7 @@ public class GeneratorTest {
 	 */
 	@Test
 	public void testRotateBufferedImage() {
-		assertEquals(i, testGenerator.rotateImage(i, 0.0));
+		assertEquals(picture, testGenerator.rotateImage(picture, 0.0));
 	}
 
 	/**
@@ -78,7 +117,7 @@ public class GeneratorTest {
 	 */
 	@Test(expected = IllegalArgumentException.class)
 	public void testRotateImageIllegalArgumentException() {
-		testGenerator.rotateImage(i, 0.7);
+		testGenerator.rotateImage(picture, 0.7);
 	}
 
 	/**
@@ -86,9 +125,9 @@ public class GeneratorTest {
 	 */
 	@Test
 	public void testRotateBufferedImage90Degrees() {
-		BufferedImage result = testGenerator.rotateImage(i, Math.toRadians(90));
-		assertEquals(i.getHeight(), result.getWidth());
-		assertEquals(i.getWidth(), result.getHeight());
+		BufferedImage result = testGenerator.rotateImage(picture, Math.toRadians(90));
+		assertEquals(picture.getHeight(), result.getWidth());
+		assertEquals(picture.getWidth(), result.getHeight());
 	}
 
 	/**
@@ -96,8 +135,8 @@ public class GeneratorTest {
 	 */
 	@Test
 	public void testRotateBufferedImage270Degrees() {
-		BufferedImage result = testGenerator.rotateImage(i, Math.toRadians(270));
-		assertEquals(i.getHeight(), result.getWidth());
-		assertEquals(i.getWidth(), result.getHeight());
+		BufferedImage result = testGenerator.rotateImage(picture, Math.toRadians(270));
+		assertEquals(picture.getHeight(), result.getWidth());
+		assertEquals(picture.getWidth(), result.getHeight());
 	}
 }
