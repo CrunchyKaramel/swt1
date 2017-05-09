81a261d785f88599d57ab0312acd2be9cdbbae21
Joshua Eilebrecht <joshua_eilebrecht@web.de>
2017-05-09 10:16:53 +0200

    Added GeneratorTest
    added GeneratorTest to test more methods of Generator. Test coverage should be at least be 35%.

---
+++ iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
@@ -0,0 +1,16 @@
+package org.jis.generator;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class GeneratorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
