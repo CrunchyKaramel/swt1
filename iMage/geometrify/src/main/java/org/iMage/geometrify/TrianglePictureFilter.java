aba50a90d3e8d178245b475647d44ca253d8d722
Joshua Eilebrecht <joshua_eilebrecht@web.de>
2017-05-22 19:57:13 +0200

    Completed calculateColor in TrianglePictureFilter
    calculateColor now calculates the mixed color of all pixels in the primitive (triangle).

--- iMage/geometrify/src/main/java/org/iMage/geometrify/TrianglePictureFilter.java
+++ iMage/geometrify/src/main/java/org/iMage/geometrify/TrianglePictureFilter.java
@@ -1,6 +1,7 @@
 package org.iMage.geometrify;
 
 import java.awt.Color;
+import java.awt.Point;
 import java.awt.image.BufferedImage;
 
 public class TrianglePictureFilter extends AbstractPrimitivePictureFilter {
@@ -11,10 +12,30 @@ public class TrianglePictureFilter extends AbstractPrimitivePictureFilter {
 
 	@Override
 	protected Color calculateColor(BufferedImage image, IPrimitive primitive) {
-		/*
-		 * YOUR SOLUTION HERE
-		 */
-		return null;
+		Color color = null;
+		for (int a = primitive.getBoundingBox().getUpperLeftCorner().x; a <= primitive.getBoundingBox()
+				.getLowerRightCorner().x; a++) {
+			for (int b = primitive.getBoundingBox().getUpperLeftCorner().y; b <= primitive.getBoundingBox()
+					.getLowerRightCorner().y; b++) {
+				if (primitive.isInsidePrimitive(new Point(a, b))) {
+					if (color == null) {
+						color = new Color(image.getRGB(a, b));
+					} else {
+						Color newColor = new Color(image.getRGB(a, b));
+						double totalAlpha = color.getAlpha() + newColor.getAlpha();
+						double weight0 = color.getAlpha() / totalAlpha;
+						double weight1 = newColor.getAlpha() / totalAlpha;
+
+						double c = weight0 * color.getRed() + weight1 * newColor.getRed();
+						double d = weight0 * color.getGreen() + weight1 * newColor.getGreen();
+						double e = weight0 * color.getBlue() + weight1 * newColor.getBlue();
+						double f = Math.max(color.getAlpha(), newColor.getAlpha());
+						color = new Color((int) c, (int) d, (int) e, (int) f);
+					}
+				}
+			}
+		}
+		return color;
 	}
 
 	@Override
