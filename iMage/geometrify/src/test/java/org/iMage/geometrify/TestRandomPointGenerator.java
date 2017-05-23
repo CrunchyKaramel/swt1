2f35441aeaa64cf5bc920bfe5148353ab409b412
Joshua Eilebrecht <joshua_eilebrecht@web.de>
2017-05-23 17:00:50 +0200

    Fixed code style errors
    Fixed some code style errors detected by checkstyle. This includes:
    -moving the class to a better package
    -adding comments to every method and to the class itself.

--- iMage/geometrify/src/test/java/org/iMage/geometrify/TestRandomPointGenerator.java
+++ iMage/geometrify/src/test/java/org/iMage/geometrify/TestRandomPointGenerator.java
@@ -1,34 +1,45 @@
-package iMage.geometrify;
+package org.iMage.geometrify;
 
 import java.awt.Point;
 
-import org.iMage.geometrify.RandomPointGenerator;
-import org.junit.After;
 import org.junit.AfterClass;
-import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+/**
+ * Test to check if RandomPointGenerator can create 1000 valid points.
+ * 
+ * @author Joshua Eilebrecht
+ * @version 1.0
+ *
+ */
 public class TestRandomPointGenerator {
 	static RandomPointGenerator generator;
 
+	/**
+	 * creates a new generator with a preset size.
+	 * 
+	 * @throws Exception
+	 *             any kind of exception
+	 */
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 		generator = new RandomPointGenerator(547, 398);
 	}
 
+	/**
+	 * tears down everything.
+	 * 
+	 * @throws Exception
+	 *             any kind of exception
+	 */
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
 	}
 
-	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
+	/**
+	 * tests whether RandomPointGenerator can create 1000 valid points.
+	 */
 	@Test
 	public void testNextPoint() {
 		for (int a = 0; a < 1000; a++) {
