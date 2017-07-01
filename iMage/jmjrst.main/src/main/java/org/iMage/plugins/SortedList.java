847a8dc3b5c7bbdd41b2f0d0ddbd35f9003641d5
Joshua Eilebrecht <joshua_eilebrecht@web.de>
2017-07-01 17:09:09 +0200

    Added SortedList
    Added the Iterable class SoertedList and thus completing the SortedList.

---
+++ iMage/jmjrst.main/src/main/java/org/iMage/plugins/SortedList.java
@@ -0,0 +1,32 @@
+package org.iMage.plugins;
+
+import java.util.Iterator;
+
+/**
+ * a list required for PluginList.
+ * 
+ * @author Joshua Eilebrecht
+ *
+ * @param <T>
+ *            any type of object
+ */
+public class SortedList<T> implements Iterable<T> {
+
+	private SortedListIterator<T> iter;
+
+	/**
+	 * constructor for SortedList.
+	 */
+	SortedList() {
+		this.iter = new SortedListIterator<T>();
+	}
+
+	/**
+	 * gives the iterator for this list.
+	 */
+	@Override
+	public Iterator<T> iterator() {
+		return iter;
+	}
+
+}
