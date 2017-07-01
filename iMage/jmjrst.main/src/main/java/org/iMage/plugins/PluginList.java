d336eb763e647bad5ae2f632e2bc1a10c735d923
Joshua Eilebrecht <joshua_eilebrecht@web.de>
2017-07-01 17:09:45 +0200

    Added PluginList
    Added the class PluginList, which is a specialized type of SortedList, which only accepts JmjrstPlugins.

---
+++ iMage/jmjrst.main/src/main/java/org/iMage/plugins/PluginList.java
@@ -0,0 +1,28 @@
+package org.iMage.plugins;
+
+/**
+ * a list to store plugins in.
+ * 
+ * @author Joshua Eilebrecht
+ *
+ */
+public class PluginList {
+
+	private SortedList<JmjrstPlugin> list;
+
+	/**
+	 * constructor for the PluginList.
+	 */
+	public PluginList() {
+		this.list = new SortedList<JmjrstPlugin>();
+	}
+
+	/**
+	 * gets the list/Iterable.
+	 * 
+	 * @return theiterable object of this list.
+	 */
+	public Iterable<JmjrstPlugin> getPlugins() {
+		return this.list;
+	}
+}
